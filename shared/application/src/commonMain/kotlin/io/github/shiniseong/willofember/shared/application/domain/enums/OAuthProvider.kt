72c5523551c5a1be50202026b2b64611fcf3fecf
shiniseong <hss275989@gmail.com>
2025-02-10 23:24:45 +0900

    feat: OAuthProvider 정의

---
+++ shared/application/src/commonMain/kotlin/io/github/shiniseong/willofember/shared/application/domain/enums/OAuthProvider.kt
@@ -0,0 +1,13 @@
+package io.github.shiniseong.willofember.shared.application.domain.enums
+
+enum class OAuthProvider {
+    GOOGLE,
+    FACEBOOK,
+    GITHUB,
+    KAKAO,
+    NAVER,
+    APPLE,
+    LINE,
+    TWITTER,
+    MICROSOFT,
+}
