4c4c7fa5f4f64c5418658d3378116671bc9e6e3c
shiniseong <hss275989@gmail.com>
2025-02-17 00:20:45 +0900

    feat: DateTimeString.toLocalDateTime() 정의

--- shared/application/src/commonMain/kotlin/io/github/shiniseong/willofember/shared/application/domain/util/dateTimeUtil.kt
+++ shared/application/src/commonMain/kotlin/io/github/shiniseong/willofember/shared/application/domain/util/dateTimeUtil.kt
@@ -5,4 +5,7 @@ import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
+typealias DateTimeString = String
+
 fun LocalDateTime.Companion.now(): LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+fun DateTimeString.toLocalDateTime(): LocalDateTime = LocalDateTime.parse(this)
